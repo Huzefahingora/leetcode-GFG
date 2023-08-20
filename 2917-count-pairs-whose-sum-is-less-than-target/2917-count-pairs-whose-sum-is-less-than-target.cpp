ce03b704a15364b6f35c298b9234b5a5735dce0e
Huzefa Hingora <119166843+Huzefahingora@users.noreply.github.com>
2023-08-20 11:59:44 +0530

    Time: 7 ms (57.14%), Space: 20.4 MB (14.29%) - LeetHub

--- 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
   int countPairs(vector<int>& nums, int target) {
-        int cnt = 0 ;
-        int n = nums.size();
-        multiset<int>st ;
-        for(int i = 0 ; i < n ;i++){
-            auto pos = st.lower_bound(target-nums[i]);
-            cnt += distance(st.begin(),pos);
-            st.insert(nums[i]);
-        }
-        return cnt ;
+      int ans = 0;
+      int n = nums.size();
+      for(int i = 0; i < n ; i++)
+      {
+          for(int j = i + 1; j < n; j++)
+          {
+              if(nums[i] + nums[j] < target)
+              {
+                  ans += 1;
+              }
+          }
+      }
+      return ans;
     }
 };
