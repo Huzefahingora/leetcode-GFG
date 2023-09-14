6433a91de41785cd419cc0d1acf3f75b7228ca17
Huzefa Hingora <119166843+Huzefahingora@users.noreply.github.com>
2023-09-14 22:26:29 +0530

    Time: 19 ms (72.51%), Space: 15.3 MB (36.34%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -0,0 +1,48 @@
+class Solution {
+private:
+ unordered_map<string, vector<string>> flightGraph; // Represents flights from one airport to another
+    vector<string> travelItinerary; // Stores the final travel itinerary
+
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        // Populate the flight graph using ticket information
+        for (int i = 0; i < tickets.size(); i++) {
+            string fromAirport = tickets[i][0];
+            string toAirport = tickets[i][1];
+
+            flightGraph[fromAirport].push_back(toAirport);
+        }
+        
+        // Sort destinations in reverse order to visit lexical smaller destinations first
+        for (auto &entry : flightGraph) {
+            sort(entry.second.rbegin(), entry.second.rend());
+        }
+
+        stack<string> dfsStack;
+        dfsStack.push("JFK");
+
+        while (!dfsStack.empty()) {
+            // Get the current airport from the top of the stack
+            string currentAirport = dfsStack.top();
+
+            vector<string> &destinations = flightGraph[currentAirport];
+
+            if (!destinations.empty()) {
+                // Choose the next destination (the one in lexicographically larger order)
+                string nextDestination = destinations.back();
+                destinations.pop_back();
+
+                dfsStack.push(nextDestination);
+            } else {
+                // If there are no more destinations from the current airport, add it to the itinerary
+                travelItinerary.push_back(currentAirport);
+        
+                dfsStack.pop();
+            }
+        }
+        
+        // Reverse the itinerary to get the correct order
+        reverse(travelItinerary.begin(), travelItinerary.end());
+        return travelItinerary;
+    }
+};
