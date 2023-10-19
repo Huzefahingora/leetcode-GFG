3f7cf6c2a66cde5bf51ecfe5818ea6bfccd8966d
Huzefa Hingora <119166843+Huzefahingora@users.noreply.github.com>
2023-10-19 12:01:36 +0530

    Time: 0 ms (100%), Space: 6.6 MB (54.78%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
+    string processString(string s)
+    {
+        string n = "";
+        for(char c : s)
+        {
+            if(c == '#' && !n.empty())
+            {
+                n.pop_back();
+            }
+            else if(c != '#')
+            {
+                n.push_back(c);
+            }
+        }
+        return n;
+    }
     bool backspaceCompare(string s, string t) {
-        string ns = "" , nt = "";
-        stack<char>st1,st2;
-        for(int i =0 ; i <s.length(); ++i)
-        {
-            if(s[i] != '#')
-            {
-                st1.push(s[i]);
-            }
-            else if(s[i] == '#' && st1.size()!=0)st1.pop();
-        }
-        for(int i =0 ; i <t.length(); ++i)
-        {
-            if(t[i] != '#')
-            {
-                st2.push(t[i]);
-            }
-            else if(t[i] == '#' && st2.size()!=0)st2.pop();
-        }
-        // cout << ns << endl << nt <<endl;
-        return(st1 == st2)?true:false;
+        //stack approach
+        // stack<char>st1,st2;
+        // for(int i =0 ; i <s.length(); ++i)
+        // {
+        //     if(s[i] != '#')
+        //     {
+        //         st1.push(s[i]);
+        //     }
+        //     else if(s[i] == '#' && st1.size()!=0)st1.pop();
+        // }
+        // for(int i =0 ; i <t.length(); ++i)
+        // {
+        //     if(t[i] != '#')
+        //     {
+        //         st2.push(t[i]);
+        //     }
+        //     else if(t[i] == '#' && st2.size()!=0)st2.pop();
+        // }
+        // return(st1 == st2)?true:false;
+        //itretive fast 
+        return processString(s) == processString(t);
     }
 };
